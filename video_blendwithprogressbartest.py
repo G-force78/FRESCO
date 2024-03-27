806e8fedf733829a7e2cdcae23ee818ee0ccfead
G-force78 <114336644+G-force78@users.noreply.github.com>
2024-03-27 14:14:09 +0000

    Update video_blendwithprogressbartest.py
    Signed-off-by: G-force78 <114336644+G-force78@users.noreply.github.com>

--- video_blendwithprogressbartest.py
+++ video_blendwithprogressbartest.py
@@ -37,7 +37,7 @@ if os_str == 'Windows':
     ebsynth_bin = '.\\src\\ebsynth\\deps\\ebsynth\\bin\\ebsynth.exe'
 elif os_str == 'Linux':
     ebsynth_bin = './src/ebsynth/deps/ebsynth/bin/ebsynth'
-    #os.chmod(exe_file, 0o755)
+    os.chmod('./src/ebsynth/deps/ebsynth/bin/ebsynth.exe', 0o755)
 elif os_str == 'Darwin':
     ebsynth_bin = './src/ebsynth/deps/ebsynth/bin/ebsynth.app'
 else:
